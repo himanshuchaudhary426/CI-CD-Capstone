c9181e3b1b4507611de4e715619fb539a0b30c86
Himanshu Chaudhary <himanshu.chaudhary@knoldus.com>
2020-10-16 16:34:54 +0530

    v1

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@ name="-t himanshuchaudhary/http-akka-project"
 export tag="$(git log -1 --format=%h)"
 file="-f /home/knoldus/dockerCapstone/Dockerfile"
 context="/home/knoldus/dockerCapstone/"
+user="-u himanshuchaudhary -p himanshu123@A"
 
 docker build $name:$tag $file $context
-
+docker login user
 docker push himanshuchaudhary/http-akka-project:$tag
