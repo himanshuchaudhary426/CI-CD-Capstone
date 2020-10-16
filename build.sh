788c908bc6077ef6a815bb231f6e2a854208059a
Himanshu Chaudhary <himanshu.chaudhary@knoldus.com>
2020-10-16 17:43:54 +0530

    v1

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ file="-f /home/knoldus/dockerCapstone/Dockerfile"
 context="/home/knoldus/dockerCapstone/"
 
 docker build $name:$tag $file $context
-sudo cat /home/knoldus/capstone/my_password.txt | docker login "--username" himanshuchaudhary "--password-stdin"
+
 docker push himanshuchaudhary/http-akka-project:$tag
